b4072b1b9cb8db5885d098b98ccac5b7385aeba7
linejie <linjie@isspu.com>
2016-11-10 10:23:36 +0800

    Using Loaders

---
+++ src/app.js
@@ -0,0 +1,17 @@
+cats = require('./cats.js');
+
+const addMethod = (a,b) => {
+  return a + b
+}
+
+const obj = {
+  a: 1,
+  b: 2,
+  c: 3,
+}
+
+
+const { a, b } = obj
+console.log(a,b)
+console.log(addMethod(1,1))
+console.log(cats);
